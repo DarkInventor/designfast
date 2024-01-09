f409f83dbe9f8ea90374668dc1f63f9d1ed1ab3a
DarkInventor <ktmehta25@gmail.com>
2024-01-10 04:30:30 +0530

    added stripe

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -7,7 +7,7 @@ const Layout = ({ children }) => {
         <>
             <Head>
                 <title>Designfast - Figma Design Kit</title>
-                <meta name='description' content='Accelerate your next design project with premium UI kits to get you started. Be a pro at designing interfaces that convert.' />
+                <meta name='description' content='Accelerate your next design project with premium UI kit to get you started. Be a pro at designing interfaces that convert.' />
                 <meta name='viewport' content='width=device-width, initial-scale=1' />
                 <link rel='icon' href='/favicon.ico' />
             </Head>
